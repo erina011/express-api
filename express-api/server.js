3f54b55562b15d82475c8766557c1ca26a6ef582
erina011 <157619224+erina011@users.noreply.github.com>
2025-06-24 16:50:57 +0800

    Update server.js

--- express-api/server.js
+++ express-api/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const expenseTypeRoutes = require('./routes/expenseType');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
+app.use('/api/expenses', expenseTypeRoutes);
 
-
-app.use((req, res) => {
-    res.status(404).type('application/json').json({ error: 'Route not found' });
-});
-
-
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
