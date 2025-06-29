2b5a7bce9ee3fee60f4ac130e91f8907617538f7
erina011 <157619224+erina011@users.noreply.github.com>
2025-06-29 09:44:17 +0800

    Update server.js

--- express-api/server.js
+++ express-api/server.js
@@ -6,9 +6,16 @@ const expenseTypeRoutes = require('./routes/expenseType');
 app.use(cors());
 app.use(express.json()); 
 
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type'], 
+};
+
 app.use('/api/expenses', expenseTypeRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
